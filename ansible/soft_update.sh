8f080f83d88c3f84537e308482ce3acfec4b5eae
Charles_kun <8374954@qq.com>
2016-04-18 14:38:00 +0800

    update shell

--- ansible/soft_update.sh
+++ ansible/soft_update.sh
@@ -1,4 +1,8 @@
 echo "hello"
-sudo apt-get -y update
-nl /etc/nginx/nginx.conf | sed 's/sendfile on/sendfile off/g'
+apt-get -q -y update
+apt-get -q -y upgrade
+apt-get -q -y autoremove
+apt-get -q -y autoclean
+apt-get -q -y clean
+# nl /etc/nginx/nginx.conf | sed 's/sendfile on/sendfile off/g'
 echo "end"
